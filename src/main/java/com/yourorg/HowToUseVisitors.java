62058739ce4586d9a49306dac51435d4c5ba8ff4
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-04-10 00:14:08 +0530

    Calculate 21+21

--- src/main/java/com/yourorg/HowToUseVisitors.java
+++ src/main/java/com/yourorg/HowToUseVisitors.java
@@ -1,5 +1,6 @@
 package com.yourorg;
 
+import org.openrewrite.Cursor;
 import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
@@ -22,12 +23,34 @@ public class HowToUseVisitors extends Recipe {
         return new JavaIsoVisitor<>() {
             @Override
             public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext executionContext) {
-                if(method.getSimpleName().equals("test") && method.getMethodType() != null) {
-                    return method.withName(method.getName().withSimpleName("theAnswerToLifeUniverseAndEverything"))
-                    .withMethodType(method.getMethodType().withName("theAnswerToLifeUniverseAndEverything"));
+                J.MethodDeclaration m = super.visitMethodDeclaration(method, executionContext);
+                // if(method.getSimpleName().equals("test") && method.getMethodType() != null) {
+                if(getCursor().getMessage("sum", 0) == 42) {
+                    return m.withName(m.getName().withSimpleName("theAnswerToLifeUniverseAndEverything"))
+                    .withMethodType(m.getMethodType().withName("theAnswerToLifeUniverseAndEverything"));
                 }
-                return super.visitMethodDeclaration(method, executionContext);
+
+                return m;
             }
+
+            // visitLiteral, intercepts literals 21+21
+
+            @Override
+            public J.Literal visitLiteral(J.Literal literal, ExecutionContext executionContext) {
+                Object value = literal.getValue();
+                if(value instanceof Integer) {
+                    Cursor cursor = getCursor();
+                    // Cursor{Literal->Binary->Return->JRightPadded(element=return 21+21, after=Space(comments=<0 comments>, whitespace=''))->Block->MethodDeclaration->JRightPadded(element=MethodDeclaration{Test{name=theAnswerToLifeUniverseAndEverything,return=int,parameters=[]}}, after=Space(comments=<0 comments>, whitespace=''))->Block->ClassDeclaration->CompilationUnit->root}
+                    Cursor method = cursor.dropParentUntil(J.MethodDeclaration.class::isInstance);
+                    Integer sum = method.getMessage("sum", 0);
+                    method.putMessage("sum", sum+(Integer)value);
+                }
+
+
+
+                return super.visitLiteral(literal, executionContext);
+            }
+
 
             // @Override // top level root LST element, place breakpoint and see cu
             // public J.CompilationUnit visitCompilationUnit(J.CompilationUnit cu, ExecutionContext executionContext) {
